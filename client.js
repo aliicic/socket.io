f2b13265d3b0be525a9639f9cb5b35e720265d89
aliicic <aliaghakhani911@gmail.com>
2022-08-04 16:55:20 +0430

    init simple msg-system

--- client.js
+++ client.js
@@ -1,38 +1,39 @@
-// const teachersSocket = io("ws://localhost:3000/teachers");
-// teachersSocket.on("connect", (data) => {
-//   teachersSocket.emit(
-//     "welcome-teachers-client",
-//     "hello i am teachers name space"
-//   );
-//   teachersSocket.on("welcome-teachers", (data) => {
-//     document.write(data);
-//   });
-// });
-// const studentSocket = io("ws://localhost:3000/students");
-// studentSocket.on("connect", (data) => {
-//   studentSocket.emit(
-//     "welcome-students-client",
-//     "hello i am students name space"
-//   );
-//   studentSocket.on("welcome-students", (data) => {
-//     document.write(data);
-//   });
-// });
-const socket = io("ws://localhost:3000/", {
-  query: {
-    field1: "value1",
-    field2: "value2",
-  },
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        Authorization: "Bearer <token>",
-      },
-    },
-  },
-});
+const texBox = document.getElementById("chat-content");
+const send = document.getElementById("send-btn");
+const chatBox = document.getElementById("chat-box");
+const socket = io("http://localhost:3000/");
+
+
+//? send message to server
+
 socket.on("connect", (data) => {
-  //   socket.on("broadcasting", (data) => {
-  //     document.write(data);
-  //   });
+  send.addEventListener("click", () => {
+    if (!texBox.value) return alert("field most be filled");
+    socket.emit("messageFromClient", texBox.value);
+    texBox.value = "";
+  });
+});
+
+
+let savedMessage = [];
+
+//? get message from server
+
+socket.on("messageFromServer", (data) => {
+  savedMessage.push(data);
+  localStorage.setItem("messages", savedMessage);
+  const message = document.createElement("p");
+  message.classList.add("messages");
+  message.innerHTML = data;
+  chatBox.appendChild(message);
+});
+
+//? for init previus message 
+savedMessage.push(localStorage.getItem("messages"));
+const allmsg = localStorage.getItem("messages");
+allmsg.split(",").map((i) => {
+  const message = document.createElement("p");
+  message.classList.add("messages");
+  message.innerHTML = i;
+  chatBox.appendChild(message);
 });
--- server.js
+++ server.js
@@ -10,22 +10,11 @@ const io = socketIO(server, {
   },
 });
 io.on("connection", (socket) => {
-  // socket.emit("broadcasting", "hello every one");
-  console.log(socket.handshake.query);
-  console.log(socket.handshake.headers['authorization']);
-});
+  socket.on("messageFromClient", (data) => {
+     //? io is for global sen message to all client
+      io.emit("messageFromServer", data);
 
-// io.of("/teachers").on("connection", (socket) => {
-//   socket.on("welcome-teachers-client", (data) => {
-//     console.log(data);
-//   });
-//   socket.emit("welcome-teachers", "hello teachers welcome to your server\n\n");
-// });
-// io.of("/students").on("connection", (socket) => {
-//   socket.on("welcome-students-client", (data) => {
-//     console.log(data);
-//   });
-//   socket.emit("welcome-students", "hello students welcome to your server\n\n");
-// });
+  });
+});
 
 server.listen(3000, () => console.log("server run on port 3000"));
